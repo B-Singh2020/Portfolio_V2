2b57171a23144f6ab9c74b10c4ff40d4f4b9b2da
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2024-06-09 00:39:57 -0400

    added resume

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/BSingh_Resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
